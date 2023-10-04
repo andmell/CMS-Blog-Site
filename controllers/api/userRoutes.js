6e43c7ecf7f0ea77343ac3fc4cfa7ee212283b42
ahmell <andrewmell10@gmail.com>
2023-10-04 16:58:28 -0500

    added login and logout routes for user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,5 +12,58 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = user.id;
+            req.session.logged_in = true;
+            res.status(200).json(user);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const userInfo = await User.findOne({
+      where: { username: req.body.username },
+    }); 
+    if (!userInfo) {
+      res
+        .status(400)
+        .json({ message: "Wrong username or password, please try again!" }); 
+      return;
+    }
+    const validPassword = userInfo.checkPassword(req.body.password); 
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userInfo.id; 
+      req.session.logged_in = true;
+      if (req.session.logged_in) {
+        res.json({message: "successfully logged in!"});
+      }
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
+router.get('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
