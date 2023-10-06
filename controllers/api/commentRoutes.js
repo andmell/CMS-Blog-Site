cab78c341f0fbcde6f0ef1e68ed882183caf085c
ahmell <andrewmell10@gmail.com>
2023-10-06 17:18:26 -0500

    successfully added and tested all routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,64 @@
+const { User, Comment, Post } = require("../../models");
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+    try {
+      const comments = await Comment.findAll();
+      res.status(200).json(comments);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get("/:id", async (req, res) => {
+    try {
+      const comments = await Comment.findByPk(Number(req.params.id));
+      if (!comments) {
+        return res.status(400).json({ message: "Comment not found" });
+      } else res.status(200).json(comments);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  
+  // Add withAuth helper, and ensure req.session.user_id = post.user_id
+  router.put("/:id", async (req, res) => {
+    try {
+      const comments = await Comment.findByPk(Number(req.params.id));
+      if (comments.user_id !== req.session.user_id) {
+        res.status(403).json({ message: "That comment is not yours." })
+        return;
+      }
+      const updatedComment = await Comment.update(req.body, {
+        where: {
+          id: Number(req.params.id),
+        },
+      });
+      
+      res.status(200).json(updatedComment);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // Add withAuth helper, and ensure req.session.user_id = post.user_id
+  router.delete("/:id", async (req, res) => {
+    try {
+      const comments = await Comment.findByPk(Number(req.params.id));
+      if (comments.user_id !== req.session.user_id) {
+        res.status(403).json({ message: "That comment is not yours." })
+        return;
+      }
+      if (!comments) {
+        return res.status(400).json({ message: "Comment not found" });
+      } else {
+        await comments.destroy();
+        res.status(200).json({ message: "Comment deleted." });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,99 @@
+const { User, Comment, Post } = require("../../models");
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [Comment],
+    });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const posts = await Post.findByPk(Number(req.params.id), {
+      include: [Comment],
+    });
+    if (!posts) {
+      return res.status(400).json({ message: "Post not found" });
+    } else res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// add withAuth helper
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// add withAuth helper
+router.post("/:id/comments", async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      post_id:Number(req.params.id),
+      user_id:req.session.user_id,
+    });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add withAuth helper, and ensure req.session.user_id = post.user_id
+router.put("/:id", async (req, res) => {
+  try {
+    const findPost = await Post.findByPk(Number(req.params.id));
+    console.log(req.session.user_id);
+    console.log(findPost.user_id);
+    console.log(findPost);
+    if (findPost.user_id !== req.session.user_id) {
+      res.status(403).json({ message: "That post is not yours." })
+      return;
+    }
+
+    const updatedPost = await Post.update(req.body, {
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    if (updatedPost[0] === 0) {
+      res.status(404).json({ message: "Post not found" });
+    } else res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add withAuth helper, and ensure req.session.user_id = post.user_id
+router.delete("/:id", async (req, res) => {
+  try {
+    const posts = await Post.findByPk(Number(req.params.id));
+    if (posts.user_id !== req.session.user_id) {
+      res.status(403).json({ message: "That post is not yours." })
+      return;
+    }
+    if (!posts) {
+      return res.status(404).json({ message: "Post not found" });
+    } else {
+      await posts.destroy();
+      res.status(200).json({ message: "Post deleted." });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
